7e6c3e1850f8f8fe7db6b96690fac485a4b5429b
tuhinxyz <tuhin123op@gmail.com>
2024-08-23 12:15:02 +0530

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,16 @@
+import Header from './Header/Header.jsx'
+import Footer from './Footer/Footer.jsx'
+import Container from './container/Container.jsx'
+import Logo from './Logo.jsx';
+import LogoutBtn from './Header/LogoutBtn.jsx';
+import Button from './Button.jsx'
+import RTE from './RTE.jsx';
+import Signup from './Signup.jsx';
+import Login  from './Login.jsx';
+import PostForm from './post-form/PostForm'
+import PostCard from './PostCard.jsx'
+import AuthLayout from './AuthLayout.jsx'
+import Input from './Input.jsx'
+import Select from './Select.jsx'
+
+export {AuthLayout,Select,Input,PostCard,PostForm,Login,Signup,Header,Footer,Container,Logo,LogoutBtn,Button,RTE};
---
+++ src/pages/AllPosts.jsx
@@ -0,0 +1,57 @@
+// import React,{useState,useEffect} from 'react'
+// import appwriteService from '../components'
+// import {Container, PostCard} from '../components/index.js'
+
+import React, {useState, useEffect} from 'react'
+import { Container, PostCard } from '../components'
+import appwriteService from "../appwrite/config";
+
+function AllPosts() {
+    const [posts, setPosts] = useState([])
+    useEffect(() => {}, [])
+    appwriteService.getPosts([]).then((posts) => {
+        if (posts) {
+            setPosts(posts.documents)
+        }
+    })
+  return (
+    <div className='w-full py-8'>
+        <Container>
+            <div className='flex flex-wrap'>
+                {posts.map((post) => (
+                    <div key={post.$id} className='p-2 w-1/4'>
+                        <PostCard {...post} />
+                    </div>
+                ))}
+            </div>
+            </Container>
+    </div>
+  )
+}
+
+export default AllPosts
+
+// function AllPost() {
+//     const [posts, setPosts] = useState([])
+//     useEffect(()=>{},[])
+//     appwriteService.getPosts([]).then((posts) => {
+//         if(posts){
+//             setPosts(posts.document)
+//         }
+//     })
+//   return (
+//     <div className="w-full py-8">
+//         <Container>
+//             <div className="flex flex-wrap">
+//             {posts.map((post)=>{
+//                 <div key={post.$id} className='p-2 w-1'>
+//                     <PostCard post={post} />
+//                 </div>
+//             })}
+//             </div>
+//         </Container>
+//     </div>
+//   )
+// }
+
+// export default AllPost
