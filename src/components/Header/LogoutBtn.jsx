95ff3097d053fd87154c7822c15186793cdbe96e
Tuhin Mandal <109417986+tuhinxyz@users.noreply.github.com>
2024-08-25 10:46:39 +0530

    Update LogoutBtn.jsx

--- src/components/Header/LogoutBtn.jsx
+++ src/components/Header/LogoutBtn.jsx
@@ -10,8 +10,9 @@ function LogoutBtn() {
     const logoutHandler = () => {
         authService.logout().then(() => {
             dispatch(logout())
+             location.reload();
         })
-        location.reload();
+       
     }
   return (
     <button className='inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full' onClick={logoutHandler}>Logout</button>
