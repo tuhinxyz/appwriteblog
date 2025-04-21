29862d1a68812694162aeb2ffc342cc4a30f186d
Tuhin Mandal <109417986+tuhinxyz@users.noreply.github.com>
2025-04-21 22:30:57 +0530

    Update PostForm.jsx

--- src/components/post-form/PostForm.jsx
+++ src/components/post-form/PostForm.jsx
@@ -101,7 +101,7 @@ export default function PostForm({ post }) {
                 {post && (
                     <div className="w-full mb-4">
                         <img
-                            src={appwriteService.getFilePreview(post.featuredImage)}
+                            src={appwriteService.getFilePreview(post.featuredImage)+`&mode=admin`}
                             alt={post.title}
                             className="rounded-lg"
                         />
